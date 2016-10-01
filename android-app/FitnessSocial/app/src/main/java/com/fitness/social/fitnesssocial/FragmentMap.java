14c4ba7ee71dcc606a366c4f2e8961c156dc4058
Alvin Yan <apyan@ucsd.edu>
2016-10-01 04:48:57 -0700

    Facebook/Google GPS Android
    Following has been implemented by not completed

--- android-app/FitnessSocial/app/src/main/java/com/fitness/social/fitnesssocial/FragmentMap.java
+++ android-app/FitnessSocial/app/src/main/java/com/fitness/social/fitnesssocial/FragmentMap.java
@@ -1,29 +1,190 @@
 package com.fitness.social.fitnesssocial;
 
 
+import android.graphics.BitmapFactory;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * A fragment class used for GPS/Map services.
  */
-public class FragmentMap extends Fragment {
+public class FragmentMap extends SupportMapFragment implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, GoogleMap.OnInfoWindowClickListener,
+        GoogleMap.OnMapLongClickListener, GoogleMap.OnMapClickListener, GoogleMap.OnMarkerClickListener {
 
+    // Fragment Variables
+    private GoogleApiClient mGoogleApiClient;
+    private Location mCurrentLocation;
+
+    private final int[] MAP_TYPES = { GoogleMap.MAP_TYPE_SATELLITE,
+            GoogleMap.MAP_TYPE_NORMAL,
+            GoogleMap.MAP_TYPE_HYBRID,
+            GoogleMap.MAP_TYPE_TERRAIN,
+            GoogleMap.MAP_TYPE_NONE };
+    private int curMapTypeIndex = 0;
 
     public FragmentMap() {
         // Required empty public constructor
     }
 
+    private void initCamera( Location location ) {
+        CameraPosition position = CameraPosition.builder()
+                .target( new LatLng( location.getLatitude(), location.getLongitude() ) )
+                .zoom( 16f )
+                .bearing( 0.0f )
+                .tilt( 0.0f )
+                .build();
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        getMap().animateCamera( CameraUpdateFactory.newCameraPosition( position ), null );
+
+        getMap().setMapType( MAP_TYPES[curMapTypeIndex] );
+        getMap().setTrafficEnabled( true );
+        getMap().setMyLocationEnabled( true );
+        getMap().getUiSettings().setZoomControlsEnabled( true );
     }
 
+    /*@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        setHasOptionsMenu(true);
+
+        mGoogleApiClient = new GoogleApiClient.Builder( getActivity() )
+                .addConnectionCallbacks( this )
+                .addOnConnectionFailedListener( this )
+                .addApi( LocationServices.API )
+                .build();
+
+        //initListeners();
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }*/
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setHasOptionsMenu(true);
+
+        mGoogleApiClient = new GoogleApiClient.Builder( getActivity() )
+                .addConnectionCallbacks( this )
+                .addOnConnectionFailedListener( this )
+                .addApi( LocationServices.API )
+                .build();
+
+        initListeners();
+    }
+
+    private void initListeners() {
+        getMap().setOnMarkerClickListener(this);
+        getMap().setOnMapLongClickListener(this);
+        getMap().setOnInfoWindowClickListener( this );
+        getMap().setOnMapClickListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if( mGoogleApiClient != null && mGoogleApiClient.isConnected() ) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation( mGoogleApiClient );
+
+        initCamera( mCurrentLocation );
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        //handle play services disconnecting if location is being constantly used
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        //Create a default location if the Google API Client fails. Placing location at Googleplex
+        mCurrentLocation = new Location( "" );
+        mCurrentLocation.setLatitude( 37.422535 );
+        mCurrentLocation.setLongitude( -122.084804 );
+        initCamera(mCurrentLocation);
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        Toast.makeText( getActivity(), "Clicked on marker", Toast.LENGTH_SHORT ).show();
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        marker.showInfoWindow();
+        return true;
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+
+        MarkerOptions options = new MarkerOptions().position( latLng );
+        options.title( getAddressFromLatLng( latLng ) );
+
+        options.icon( BitmapDescriptorFactory.defaultMarker( ) );
+        getMap().addMarker( options );
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        MarkerOptions options = new MarkerOptions().position( latLng );
+        options.title( getAddressFromLatLng(latLng) );
+
+        options.icon( BitmapDescriptorFactory.fromBitmap(
+                BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher ) ) );
+
+        getMap().addMarker(options);
+    }
+
+    private String getAddressFromLatLng( LatLng latLng ) {
+        Geocoder geocoder = new Geocoder( getActivity() );
+
+        String address = "";
+        try {
+            address = geocoder.getFromLocation( latLng.latitude, latLng.longitude, 1 ).get( 0 ).getAddressLine( 0 );
+        } catch (IOException e ) {
+        }
+
+        return address;
+    }
 }
--- android-app/FitnessSocial/app/src/main/java/com/fitness/social/fitnesssocial/SplashScreen.java
+++ android-app/FitnessSocial/app/src/main/java/com/fitness/social/fitnesssocial/SplashScreen.java
@@ -5,13 +5,55 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class SplashScreen extends Activity {
+
+    // Activity Variables
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_splash_screen);
+
+        loginButton = (LoginButton)findViewById(R.id.fb_login_button);
+
+        // Facebook Initializer
+        callbackManager = CallbackManager.Factory.create();
+        AppEventsLogger.activateApp(this);
+
+        // Facebook Login Button Results
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(getApplicationContext(), "Login successfully.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getBaseContext(), MainScreen.class);
+                //intent.putExtra("EXTRA_SESSION_ID", sessionId);
+                startActivity(intent);
+            }
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "Login cancelled.", Toast.LENGTH_SHORT).show();
+
+            }
+            @Override
+            public void onError(FacebookException e) {
+                Toast.makeText(getApplicationContext(), "Unable to login.", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
     // TEST: Proceeds onto Main Screen
