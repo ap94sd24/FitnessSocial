ec48a453486cda8bf2116348d05c3bd2dc12e21a
Alvin Yan <apyan@ucsd.edu>
2016-10-01 02:02:58 -0700

    Android Design
    Basic Design Implemented

--- android-app/FitnessSocial/app/src/main/java/com/fitness/social/fitnesssocial/MainScreen.java
+++ android-app/FitnessSocial/app/src/main/java/com/fitness/social/fitnesssocial/MainScreen.java
@@ -1,13 +1,37 @@
 package com.fitness.social.fitnesssocial;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainScreen extends AppCompatActivity {
+public class MainScreen extends FragmentActivity {
+
+    // Activity Variables
+    ViewPager pager;
+    TabLayout tabLayout;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        context = this;
+        pager= (ViewPager) findViewById(R.id.view_pager);
+        tabLayout= (TabLayout) findViewById(R.id.tab_layout);
+
+        // Fragment manager to add fragment in viewpager we will pass object of Fragment manager to adapter class.
+        FragmentManager manager= getSupportFragmentManager();
+        PagerAdapter adapter=new PagerAdapter(manager, context);
+        pager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(pager);
+        pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setTabsFromPagerAdapter(adapter);
     }
 }
--- android-app/FitnessSocial/app/src/main/java/com/fitness/social/fitnesssocial/SplashScreen.java
+++ android-app/FitnessSocial/app/src/main/java/com/fitness/social/fitnesssocial/SplashScreen.java
@@ -1,9 +1,12 @@
 package com.fitness.social.fitnesssocial;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +14,11 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
     }
 
+    // TEST: Proceeds onto Main Screen
+    public void onProceed(View view) {
+        Intent intent = new Intent(getBaseContext(), MainScreen.class);
+        //intent.putExtra("EXTRA_SESSION_ID", sessionId);
+        startActivity(intent);
+    }
 
 }
